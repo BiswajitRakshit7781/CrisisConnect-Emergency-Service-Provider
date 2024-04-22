75226abaf021ecc2dff5719ebfbddbf0f781de55
BiswajitRakshit7781 <142657794+BiswajitRakshit7781@users.noreply.github.com>
2024-04-22 21:19:30 +0530

    22.04

--- CrisisConnect/src/App.jsx
+++ CrisisConnect/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import Dashboard from './components/Dashboard'
 import './App.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   const route = createBrowserRouter([
     {
       path : "/dashboard",
       element : <Dashboard/>
+    },
+    {
+      path : "/",
+      element : <Home/>
     },
   ])
 
---
+++ CrisisConnect/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center text-6xl'>
+      <a href="/dashboard">Go to dashboard</a>
+    </div>
+  )
+}
+
+export default Home
