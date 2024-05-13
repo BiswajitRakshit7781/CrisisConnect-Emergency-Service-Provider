7c70177692038571b0be9440622509cbfcc076b9
BiswajitRakshit7781 <biswajitrakshit2003@gmail.com>
2024-05-13 13:17:34 +0530

    pass

--- CrisisConnect/src/components/ForgotPassword.jsx
+++ CrisisConnect/src/components/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from './Footer'
 import { NavLink } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
@@ -8,9 +8,17 @@ const ForgotPassword = () => {
     const {
         register,
         handleSubmit,
-        watch,
-        formState: { errors },
-      } = useForm()
+        setError,
+        clearErrors,
+        formState: { errors, isSubmitting },
+    } = useForm()
+
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleHideAlert = () => {
+        setIsVisible(false);
+        clearErrors();
+    };
 
     return (
         <>
@@ -25,18 +33,40 @@ const ForgotPassword = () => {
             </nav>
 
             <main className='lgbg flex flex-col justify-center items-center'>
+
+                <div id='alert' className="alert flex w-full flex-col items-center">
+                    {errors.email && isVisible && (
+                        <div>
+                            <p>Email ID is required !</p>
+                            <button onClick={handleHideAlert} className='closebtn'>X</button>
+                        </div>
+                    )}
+                    {errors.otp && isVisible && (
+                        <div>
+                            <p>{errors.otp.message}</p>
+                            <button onClick={handleHideAlert} className='closebtn'>X</button>
+                        </div>
+                    )}
+                    {errors.password && isVisible && (
+                        <div>
+                            <p>{errors.password.message}</p>
+                            <button onClick={handleHideAlert} className='closebtn'>X</button>
+                        </div>
+                    )}
+                </div>
+
                 <div className="loginform flex flex-col items-center gap-9 ">
                     <h1>Forgot Password</h1>
                     <form className='lgform flex flex-col gap-5 items-center justify-center'>
-                        <input type="email" {...register("email", { required: true })} placeholder='Enter Your Email Address'/>
+                        <input type="email" {...register("email", { required: true })} placeholder='Enter Your Email Address' />
 
                         <button className='otp'>Send OTP</button>
 
-                        <input type="number" {...register("otp", { required: { value : true, message: "OTP is required !"} ,minLength: { value: 6, message: "Minimum 6 Character required" } })} placeholder='Enter Your OTP'/>
+                        <input type="number" {...register("otp", { required: { value: true, message: "OTP is required !" }, minLength: { value: 6, message: "Minimum 6 Character required" } })} placeholder='Enter Your OTP' />
 
                         <button className="verify">Verify</button>
 
-                        <input type="password" placeholder='Enter New Password' />
+                        <input type="password" {...register("password", { required: { value: true, message: "Password is required !" }, minLength: { value: 8, message: "Minimum 8 Character required" } })} placeholder='Set New Password' />
 
                         <input type="submit" value="Save" />
                     </form>
